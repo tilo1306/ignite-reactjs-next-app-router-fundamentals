118f14da738704fbd9ab94d3ef3749072259d3fe
Douglas dos Santos Oliveira <doug1306@gmail.com>
2023-10-17 17:48:41 -0300

    feat: end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
-import Image from 'next/image'
+import { Suspense } from 'react'
+import { LongWaitComponent } from './components/long-wait-component'
+import { GithubProfile } from './components/github-profile'
 
 export default function Home() {
-  return <h1>Hello world!</h1>
+  return (
+    <div>
+      <h1>Home!</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum provident
+        recusandae quas magnam distinctio adipisci officiis sapiente molestiae
+        libero, illo ut deserunt velit fugiat, temporibus veniam, pariatur ipsam
+        obcaecati. Hic!
+      </p>
+
+      <Suspense fallback={<p>Carregando LongWaitComponent</p>}>
+        <LongWaitComponent />
+      </Suspense>
+
+      <Suspense fallback={<p>Carregando GithubProfile</p>}>
+        <GithubProfile />
+      </Suspense>
+    </div>
+  )
 }
